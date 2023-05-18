705dcdaaf89897aa5a6f39d0737189a123b11530
Tufael Ahmed <tufaelahmed1110@gmail.com>
2023-05-19 01:38:08 +0600

    selection sort added

---
+++ sortingalgo/selectionSort.js
@@ -0,0 +1,15 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) {
+        lowest = j;
+      }
+    }
+    let temp = arr[i];
+    arr[i] = arr[lowest];
+    arr[lowest] = temp;
+  }
+  return arr;
+}
+console.log(selectionSort([13, 7, 3, -2, 5]));
