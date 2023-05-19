7dbd967b29aa7813677472d4baa13be941cf2f90
Tufael Ahmed <tufaelahmed1110@gmail.com>
2023-05-20 02:06:14 +0600

    insertion sort added on the bucket

---
+++ sortingalgo/insertionSort.js
@@ -0,0 +1,13 @@
+function insertionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (arr[j] < arr[j - 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+console.log(insertionSort([7, -2, 3, 1, 11, 8]));
